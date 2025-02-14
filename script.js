805992872009ace161eff649f6552d7eac35658c
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-02-14 21:06:07 +0100

    sprint

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ for (let i = 1; i <= 20; i++) {
     console.log(`This loop don run ${i} time`);
   }
 }
+const ages = [2, 3, 5, 7, 8];
+for (let age = 0; age <= ages.length - 1; age++) {
+  console.log(ages[age]);
+}
+const double = ages.map((age) => age * 2);
+console.log(double);
