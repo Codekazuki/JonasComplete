3f511369081b4a59b9d8c2b4c278b1fc3c3040e9
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-02-15 20:33:12 +0100

    sprint

--- script.js
+++ script.js
@@ -9,5 +9,12 @@ const ages = [2, 3, 5, 7, 8];
 for (let age = 0; age <= ages.length - 1; age++) {
   console.log(ages[age]);
 }
-const double = ages.map((age) => age * 2);
-console.log(double);
+
+const fetchDtata = async () => {
+  try {
+    const resp = await fetch();
+    const data = await resp.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
