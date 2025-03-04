da7dcf212c5d8c0f0e9bab8de94d9a3f1db866a9
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-04 23:44:40 +0100

    sprint

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
-const arr = [2, 3, 4, 5, 6];
-console.log(arr);
-console.log(arr.reverse());
-console.log(arr);
-const arr2 = [7, 8, 9];
-console.log([...arr, ...arr2]);
-console.log(arr.concat(arr2));
-console.log(arr);
+console.log("Tope my nigga");
+console.log(7);
+console.log(10 + 6 * 4);
+console.log(3 + 6 - 8 * 2);
+// variable -------cONTAINER
+// value....... content
+
+let firstName = "Yetunde";
+console.log(firstName);
+let country = "canada";
+console.log(country);
+
+country = "USA";
+country = null;
+console.log(country);
+let bucket = "coke";
+bucket = "fanta";
+// camelCase naming
+let myFirstSon = "Mercy";
+let mobile1 = "hello";
+
+// $,letters,numbers,_
+let myCountry = "UK";
+myCountry = 89;
+// 1. Number 8
+// 2. string 'this is a string '
+// 3. boolean true or false
+// 4. undefined
+let club;
+// 5. null
+// 6. symbol
+// 7.bigint
+console.log(typeof myCountry);
+
+// let,const,var
+const myYearOfBirth = 2008;
+// myYearOfBirth = 2012;
+
+console.log(2 - 4);
+// + is used for concatenation
+
+const myFirstName = "Ayodele";
+const myNickName = "Coach";
+console.log(myFirstName + " " + myNickName);
+let a, b;
+a = 50;
+b = 40;
+console.log(a, b);
+console.log((a + b) / 2);
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+console.log(BMIJohn, BMIMark);
+const markHigherBMI = BMIJohn > BMIMark;
+console.log(markHigherBMI);
