eeaeb441a5c1007a3e0c157dc4af3976fb474e51
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-10 15:44:37 +0100

    sprint

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const countChar = (word, char) => {
   return word.toLowerCase().split(char).length - 1;
 };
 console.log(countChar("javA", "a"));
+const numbers = [1, 2, 3, 4];
+const abc = numbers.filter((item) => (item = 2));
+console.log(abc);
+const addUp = numbers.reduce((acc, cur, i) => {
+  console.log(`Itiration ${i}....curr = ${acc}`);
+  return acc + cur;
+}, 0);
+console.log(addUp);
