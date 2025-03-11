cd683b29e271a74baa8b3d0523a4a3963bd3ce3f
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-11 12:42:46 +0100

    sprint

--- script.js
+++ script.js
@@ -8,12 +8,29 @@ const init = () => {
 init();
 
 const cutFruit = (fruit) => {
-  return fruit * 4;
+  return fruit / 0.25;
 };
 
 const makeJuice = (mango, pawpaw) => {
   const mangoPieces = cutFruit(mango);
   const pawpawPieces = cutFruit(pawpaw);
-  console.log(`Juice with ${mangoPieces} mango and ${pawpawPieces} pawpaw`);
+  console.log(
+    `Juice with ${mangoPieces} pieces of mango and ${pawpawPieces} pieces of pawpaw`
+  );
 };
 makeJuice(150, 250);
+
+const numbers = [5, 54, 23, 5, 18];
+const addup = numbers.reduce((acc, sum, i, arr) => {
+  return (acc += sum);
+}, 0);
+console.log(addup);
+const greaterthan20 = numbers.filter((el) => {
+  return el > 20;
+});
+console.log(greaterthan20);
+
+const addition = numbers.reduce((acc, summ) => {
+  return (acc = acc + summ);
+}, 0);
+console.log(addition);
