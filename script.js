06b9e594469831b904229e2afa4b3ddd123f793c
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-14 22:46:04 +0100

    sprint

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 // DOM document object model
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector(".check").addEventListener("click", () => {
   const userGuess = Number(document.querySelector(".guess").value);
 
-  if (!userGuess) {
+  if (!userGuess || userGuess > 20) {
     document.querySelector(".message").innerHTML =
-      "Input a valid number in the box";
+      "You never put better number";
+    document.querySelector(".message").style.backgroundColor = "red";
+    document.querySelector(".ins").style.display = "block";
   } else if (userGuess === secretNumber) {
-    document.querySelector(".message").innerHTML = "You don get am";
-  } else if (userGuess > 20) {
     document.querySelector(".message").innerHTML =
-      " eye dey pain you? we say between 1 and 20";
+      "You are smart .... ✅✅🏆🏆✅✅";
+    document.querySelector("body").style.backgroundColor = "#D9E7E2";
+    document.querySelector(".hiddenNumber").innerHTML = secretNumber;
+    document.querySelector(".playAgain").style.display = "block";
   } else if (userGuess > secretNumber) {
-    document.querySelector(".message").innerHTML = "guess too high";
+    document.querySelector(".message").innerHTML =
+      "Your head dry ...... Number is too high";
   } else if (userGuess < secretNumber) {
-    document.querySelector(".message").innerHTML = "guess too low";
+    document.querySelector(".message").innerHTML = "Dry head ,..... too low";
   }
+
+  document.querySelector(".guess").value = "";
+
+  document.querySelector(".playAgain").addEventListener("click", () => {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector(".message").innerHTML = "Start guessing";
+    document.querySelector("body").style.backgroundColor = "";
+    document.querySelector(".playAgain").style.display = "none";
+    document.querySelector(".hiddenNumber").innerHTML = "???";
+  });
 });
