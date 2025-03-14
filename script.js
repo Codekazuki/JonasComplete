40e78883a0af239bc79e9d0aaa0198984219695d
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-14 09:14:49 +0100

    sprint

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 document.querySelector(".check").addEventListener("click", () => {
-  const userGuess = Number(document.querySelector(".guess").value);
+  let userGuess = Number(document.querySelector(".guess").value);
 
   if (!userGuess) {
     document.querySelector(".message").innerHTML =
@@ -10,6 +10,13 @@ document.querySelector(".check").addEventListener("click", () => {
     document.querySelector(".message").innerHTML = "You don get am";
     document.querySelector("body").style.backgroundColor = "green";
     document.querySelector(".secNum").innerHTML = secretNumber;
+    document.querySelector(".again").style.display = "block";
+    document.querySelector(".again").addEventListener("click", () => {
+      secretNumber = Math.trunc(Math.random() * 20) + 1;
+      document.querySelector(".message").innerHTML = "Start guessing";
+      document.querySelector("body").style.backgroundColor = "";
+      document.querySelector(".secNum").innerHTML = "?";
+    });
   } else if (userGuess > 20) {
     document.querySelector(".message").innerHTML =
       " eye dey pain you? we say between 1 and 20";
@@ -18,4 +25,5 @@ document.querySelector(".check").addEventListener("click", () => {
   } else if (userGuess < secretNumber) {
     document.querySelector(".message").innerHTML = "guess too low";
   }
+  document.querySelector(".guess").value = "";
 });
