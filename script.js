9cc9246646a35d76d7df0d1413427bbd1f383671
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-25 21:09:36 +0100

    sprint

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 "use strict";
+
+//short circuiting
+// || operator ...... it returns the first truthy value, or the last value if none is true
+
+console.log(undefined || null || NaN || 0 || "");
+
+// && operator returns the first falsy value or the last value if all are true
+
+// console.log(34 && true);
+
+let isAdmin = "no";
+let isEditor = NaN;
+// console.log(isAdmin || isEditor);
+let isLoggedIn = "false";
+let isVerified = undefined;
+let haveAccess = NaN;
+console.log(haveAccess && isVerified && isAdmin && "Access granted");
