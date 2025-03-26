0230b68a4b671a7f8659b21da728839deb3ff2bd
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-26 16:46:15 +0100

    sprint

--- script.js
+++ script.js
@@ -16,4 +16,57 @@ let isLoggedIn = "false";
 let isVerified = undefined;
 let haveAccess = NaN;
 console.log(haveAccess && isVerified && isAdmin && "Access granted");
+// const objNumbers = {
+//   1: 1,
+//   2: 2,
+//   3: 3,
+//   4: 4,
+//   5: 4,
+// };
+// console.log(objNumbers);
+//for loop
+for (let i = 1; i < 11; i++) {
+  console.log(`done ${i} times`);
+}
+//while loop
+let rep = 1;
+while (rep < 11) {
+  console.log(`done ${rep} number of times`);
+  rep++;
+}
 const numbers = [1, 2, 3, 4, 4];
+
+// for-of loop
+for (const num of numbers.entries()) {
+  console.log(`position ${num[0] + 1} is ${num[1]}`);
+}
+console.log(numbers);
+console.log([...numbers.entries()]);
+
+const eplTeams = [
+  "Arsenal",
+  "Aston Villa",
+  "Bournemouth",
+  "Brentford",
+  "Brighton & Hove Albion",
+  "Burnley",
+  "Chelsea",
+  "Crystal Palace",
+  "Everton",
+  "Fulham",
+  "Liverpool",
+  "Luton Town",
+  "Manchester City",
+  "Manchester United",
+  "Newcastle United",
+  "Nottingham Forest",
+  "Sheffield United",
+  "Tottenham Hotspur",
+  "West Ham United",
+];
+for (const num of eplTeams.entries()) {
+  console.log(`position ${num[0] + 1} is ${num[1]}`);
+}
+for (let i = 2; i <= 10; i = i + 2) {
+  console.log(i);
+}
