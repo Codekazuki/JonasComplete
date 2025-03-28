4fdf81e374c3539314b93bd949614f110b6f2e61
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-03-28 12:14:51 +0100

    sprint

--- script.js
+++ script.js
@@ -8,65 +8,71 @@ console.log(undefined || null || NaN || 0 || "");
 // && operator returns the first falsy value or the last value if all are true
 
 // console.log(34 && true);
+console.log("Jesus is Lord");
+const num = [1, 2, 3];
+console.log(num.length);
 
-let isAdmin = "no";
-let isEditor = NaN;
-// console.log(isAdmin || isEditor);
-let isLoggedIn = "false";
-let isVerified = undefined;
-let haveAccess = NaN;
-console.log(haveAccess && isVerified && isAdmin && "Access granted");
-// const objNumbers = {
-//   1: 1,
-//   2: 2,
-//   3: 3,
-//   4: 4,
-//   5: 4,
+const school = "code with kazuki";
+const course = "javascript";
+console.log(course[2]);
+console.log(school.length);
+console.log(course.indexOf("a"));
+
+//slice
+console.log(school.slice(10, 16));
+console.log(course.slice(-1));
+//a & b .....science, c & d ...... commercial .... e & f ....arts
+// const checkClass = (arm) => {
+//   const individualClass = arm.slice(-1);
+//   if (individualClass === "a" || individualClass === "b") {
+//     console.log("na science class");
+//   } else if (individualClass === "c" || individualClass === "d") {
+//     console.log("Na commercial student");
+//   } else if (individualClass === "e" || individualClass === "f") {
+//     console.log("na Arts students");
+//   } else {
+//     console.log("na mechanic");
+//   }
 // };
-// console.log(objNumbers);
-//for loop
-for (let i = 1; i < 11; i++) {
-  console.log(`done ${i} times`);
-}
-//while loop
-let rep = 1;
-while (rep < 11) {
-  console.log(`done ${rep} number of times`);
-  rep++;
-}
-const numbers = [1, 2, 3, 4, 4];
+// checkClass("5b");
+// checkClass("5c");
+// checkClass("5e");
+// checkClass("5p");
+// checkClass("53");
+const checkCountry = (check) => {
+  const nationality = check.slice(-2);
+  if (nationality === "ng") {
+    console.log("Is a Nigerian");
+  } else if (nationality === "gh") {
+    console.log("Is a Ghanian");
+  } else if (nationality === "us") {
+    console.log("Is an American");
+  } else {
+    console.log("Is an Ijebu");
+  }
+};
+checkCountry("3456ng");
+checkCountry("3456gh");
+checkCountry("3456us");
+checkCountry("345346us");
 
-// for-of loop
-for (const num of numbers.entries()) {
-  console.log(`position ${num[0] + 1} is ${num[1]}`);
-}
-console.log(numbers);
-console.log([...numbers.entries()]);
+console.log(school.toLowerCase());
+console.log(school.toUpperCase());
+const country = "Nigeria";
 
-const eplTeams = [
-  "Arsenal",
-  "Aston Villa",
-  "Bournemouth",
-  "Brentford",
-  "Brighton & Hove Albion",
-  "Burnley",
-  "Chelsea",
-  "Crystal Palace",
-  "Everton",
-  "Fulham",
-  "Liverpool",
-  "Luton Town",
-  "Manchester City",
-  "Manchester United",
-  "Newcastle United",
-  "Nottingham Forest",
-  "Sheffield United",
-  "Tottenham Hotspur",
-  "West Ham United",
-];
-for (const num of eplTeams.entries()) {
-  console.log(`position ${num[0] + 1} is ${num[1]}`);
+const firstName = "   sule";
+
+const lowerYet = firstName.trim().toLowerCase();
+console.log(lowerYet[0].toUpperCase() + lowerYet.slice(1));
+const secondName = "   Abdul";
+console.log(secondName);
+console.log(secondName.trim());
+function formatName(name) {
+  const lowerYet = name.trim().toLowerCase();
+  return (
+    lowerYet[0].toUpperCase() +
+    lowerYet.slice(1, -1) +
+    lowerYet[name.length - 1].toUpperCase()
+  );
 }
-for (let i = 2; i <= 10; i = i + 2) {
-  console.log(i);
-}
+console.log(formatName("biodun"));
