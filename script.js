cf45144316dfe6392f4c7b8589eb7ec588a0a6ce
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-04-09 14:45:20 +0100

    sprint

--- script.js
+++ script.js
@@ -14,28 +14,3 @@ function two(y) {
   console.log("I am the second");
   y();
 }
-
-function three(z) {
-  console.log("I am the third");
-  z();
-}
-
-function four(a) {
-  console.log("I am the fourth");
-  a();
-}
-
-function five(b) {
-  console.log("I am the fifth");
-  b();
-}
-
-function six(c) {
-  console.log("I am the sixth");
-  c();
-}
-
-function seven() {
-  console.log("I am the seventh");
-}
-one(() => two(() => three(() => four(() => five(() => six(seven))))));
