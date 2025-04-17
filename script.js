7c011abc677c8475a797fd4191617e731de7a8cf
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-04-17 21:13:37 +0100

    sprint

--- script.js
+++ script.js
@@ -19,7 +19,15 @@ console.log("hello");
 //   .catch((err) => {
 //     console.log("This thing don lazzdent:", err);
 //   });
+const catAPI = "https://catfact.ninja/fact";
+async function getCat() {
+  let result = await fetch(catAPI);
+  let data = await result.json();
 
+  console.log(result);
+  console.log(data);
+}
+getCat();
 const checkGender = "https://api.genderize.io?name=Temitope";
 const check = fetch(checkGender)
   .then((resp) => resp.json())
